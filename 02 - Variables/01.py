64af7c5ad5de2c1398537245d81ba6aa146a91c7
Sarath <sarath.regedit@gmail.com>
2022-11-19 00:22:17 +0530

    Python variables

---
+++ 02 - Variables/01.py
@@ -0,0 +1,27 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+print(type(x))
+print(type(y))
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
